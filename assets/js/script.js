be8ea71763ce2358b1eca4402a32d72f8208956c
Debbie Hall <debleehall15@gmail.com>
2021-11-28 20:00:59 -0500

    trying something new, fixed html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,43 @@
 // Questions
-var questions = [
+const questions = [
     {
         question: "Which of these are not a coding language?",
-        answers: [
-            {text: "JavaScript", correct: false},
-            {text: "CSS", correct: false},
-            {text: "Bootstrap", correct: true},
-            {text: "HTML", correct: false},
-        ]
+        choices: ["a. JavaScript", "b. CSS", "c. Bootstrap", "d. HTML"],
+        answer: "c. Bootstrap"
     },
     {
         question: "What is the command to 'push' code to GitHub?",
-        answers: [
-            {text: "git push origin main", correct: true},
-            {text: "git push", correct: false},
-            {text: "git commit -m", correct: false},
-            {text: "push origin main", correct: false},
-        ]
+        choices: ["a. git push origin main", "b. git push", "c. git commit -m", "d. push origin main"],
+        answer: "a. git push origin main"
     },
     {
         question: "How do you create a flex box?",
-        answers: [
-            {text: "display: box;", correct: false},
-            {text: "display: flex;", correct: true},
-            {text: "display: flexbox;", correct: false},
-        ]
+        choices: ["a. display: box;", "b. display: flex;", "c. display: flexbox;", "flex: box;"],
+        answer: "b. display: flex;"
     },
     {
         question: "the condition in an if/else statement is enclosed with _______.",
-        answers: [
-            {text: "Quotes", correct: false},
-            {text: "Square brakets", correct: false},
-            {text: "Parenthesis", correct: false},
-            {text: "Curly brackets", correct: true},
-        ]
+        choices: ["a. Quotes", "b. Square brakets", "c. Parenthesis", "d. Curly brackets"],
+        answer: "d. Curly brackets"
     },
     {
         question: "Arrays in JavaScript can be used to store ______.",
-        answers: [
-            {text: "Numbers and strangs", correct: false},
-            {text: "Other arrays", correct: false},
-            {text: "Booleans", correct: false},
-            {text: "All of the above", correct: true},
-        ]
-    },
+        choices: ["a. Numbers and strangs", "b. Other arrays", "c. Booleans", "d. All of the above"],
+        answer: "d. All of the above"
+    }
 ]
 
 var startButton = document.getElementById('start-btn');
-var startScreen = document.getElementById('start');
-var questionContainer = document.getElementById('questions');
+var startScreen = document.getElementById('startScreen');
+var questionContainer = document.getElementById('questionContainer');
 var questionEl = document.getElementById('question');
-var answerButton = document.getElementById('answers');
-var endScreen = document.getElementById('end');
+var answerButton = document.getElementById('choices');
+var endScreen = document.getElementById('end-screen');
+var scoreBonus = 10;
+
+var score = 0;
+var randomQuestions;
+var setQuestions = 0;
 
 startButton.addEventListener('click', startQuiz);
 
@@ -61,7 +47,7 @@ var secondsLeft = 60;
 var timer;
 function timer() {
     count.textContent = secondsLeft;
-    timer = setInterval(function(){
+    timer = setInterval(function() {
         secondsLeft--;
         if (secondsLeft === 0) {
             clearInterval(timer);
@@ -105,6 +91,46 @@ document.querySelector("#question").textContent = question.question;
 
 //button text
 document.querySelector("#btn1").textContent = "1. " +question.answers[0].text
-document.querySelector("#btn2").textContent = "2. " +question.answers[0].text
-document.querySelector("#btn3").textContent = "3. " +question.answers[0].text
-document.querySelector("#btn4").textContent = "4. " +question.answers[0].text
+document.querySelector("#btn2").textContent = "2. " +question.answers[1].text
+document.querySelector("#btn3").textContent = "3. " +question.answers[2].text
+document.querySelector("#btn4").textContent = "4. " +question.answers[3].text
+
+var score = "1";
+document.querySelector('.btn').addEventListener('click', function() {
+    setQuestions++;
+    console.log(question.answers[0].text) 
+
+    if (question.answers[0].correct) {
+        incrementScore(scoreBonus);
+        console.log(incrementScore)
+        success()
+    } else {
+        failure()
+    }
+    generateQuestion();
+
+})
+
+incrementScore = num => {
+    score += num;
+}
+
+var answer = document.createElement('p')
+document.querySelector('#choices').appendChild(answer)
+
+function success() {
+    answer.textContent = "Correct!"
+}
+
+function failure() {
+    answer.textContent = "Wrong!"
+}
+
+// End quiz
+function endQuiz() {
+    startButton.remove()
+    startScreen.remove()
+    questionContainer.remove()
+    endScreen.classList.remove('hide');
+    clearInterval(timer);
+}
